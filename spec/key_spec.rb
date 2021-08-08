c720f21681f498c135a5373676a4d2be9e3ca30a
Jessica Jones <daddehjones@gmail.com>
2021-08-08 18:08:37 -0400

    Create Key existence and attributes.

---
+++ spec/key_spec.rb
@@ -0,0 +1,30 @@
+require_relative 'spec_helper'
+require './lib/key'
+
+RSpec.describe Key do
+  describe 'Object Creation' do
+    it 'exists' do
+      key = Key.new
+
+      expect(key).to be_a(Key)
+    end
+
+    it 'has readable attributes' do
+      key = Key.new
+
+      expect(key.key).to be_a(String)
+      expect(key.key.length).to eq(5)
+    end
+  end
+
+  describe 'Object Methods' do
+    it "can randomize keys" do
+      key = Key.new
+      
+      expect(key.randomized_key).to be_a(String)
+      expect(key.randomized_key.length).to eq(5)
+      allow(key).to receive(:randomized_key).and_return("08888")
+      expect(key.randomized_key).to eq("08888")
+    end
+  end
+end
