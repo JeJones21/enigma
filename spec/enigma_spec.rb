7d78d30cbac8d4f7ccfeb15b3844ab98a4d05cce
Jessica Jones <daddehjones@gmail.com>
2021-08-07 21:55:17 -0400

    Create attributes expectations.

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -4,9 +4,13 @@ require './lib/enigma'
 RSpec.describe Enigma do
   context 'initialize' do
     enigma = Enigma.new
-
     it "exists" do
       expect(enigma).to be_a(Enigma)
+    end
+
+    it "has attributes" do
+      expect(enigma.character_set.class).to eq(Array)
+      expect(enigma.character_set.length).to eq(27)
     end
   end
 
