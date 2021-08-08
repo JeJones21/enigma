199ebdb00566f0534f4f6af7f0f8d0ba2b9f9e03
Jessica Jones <daddehjones@gmail.com>
2021-08-08 13:30:06 -0400

    Remove character set from Enigma expectations.

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -7,12 +7,5 @@ RSpec.describe Enigma do
     it "exists" do
       expect(enigma).to be_a(Enigma)
     end
-
-    it "has attributes" do
-      expect(enigma.character_set.class).to eq(Array)
-      expect(enigma.character_set.length).to eq(27)
-    end
   end
-
-
 end
