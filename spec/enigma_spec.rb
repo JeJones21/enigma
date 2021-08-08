05d20b0fcdb2dca74befd83b2b47754a862f54d0
Jessica Jones <daddehjones@gmail.com>
2021-08-08 15:40:16 -0400

    Create current date expectations.

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -9,5 +9,10 @@ RSpec.describe Enigma do
       expect(enigma.character_set.class).to eq(Array)
       expect(enigma.character_set.length).to eq(27)
     end
+
+    it "can return today's date" do
+      expect(enigma.current_date.length).to eq(6)
+      expect(enigma.current_date.class).to eq(String)
+    end
   end
 end
