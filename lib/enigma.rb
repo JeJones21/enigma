207c5af103c85239b3062e51d6a379e2f009b9dc
Jessica Jones <daddehjones@gmail.com>
2021-08-08 15:42:22 -0400

    Require 'date'. Create current_date method.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,8 +1,13 @@
+require 'date'
 class Enigma
 
   attr_reader :character_set
 
   def initialize
     @character_set = ("a".."z").to_a << " "
+  end
+
+  def current_date
+    Date.today.strftime("%d%m%y")
   end
 end
