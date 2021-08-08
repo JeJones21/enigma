89e3eeb8f8f09062b46e763734c6bc87fab519ec
Jessica Jones <daddehjones@gmail.com>
2021-08-08 18:09:37 -0400

    Comment out Shift class.

---
+++ lib/shift.rb
@@ -0,0 +1,13 @@
+# require 'date'
+#
+# module Shiftable
+#
+#   def current_date
+#     Date.today.strftime("%d%m%y")
+#   end
+#
+#   def random_key
+#     5.times.map{rand(10)}.join
+#   end
+#
+# end
