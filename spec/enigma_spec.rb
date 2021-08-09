34616353d7556f7a96278a9b6ff81ff109d70bec
Jessica Jones <daddehjones@gmail.com>
2021-08-09 13:45:14 -0400

    Create complete shift test.

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,6 +1,8 @@
 require 'date'
 require './lib/enigma'
-# require_relative 'spec_helper'
+require './lib/key'
+require './lib/offset'
+require_relative 'spec_helper'
 
 RSpec.describe Enigma do
   context 'initialize' do
@@ -8,7 +10,10 @@ RSpec.describe Enigma do
 
     it "exists and has readable attributes" do
       expect(enigma).to be_a(Enigma)
+    end
 
+    it "can create a complete shift combining keys and offsets" do
+      expect(enigma.complete_shift([40, 02, 20, 05], [4, 0, 4, 1])).to eq([44, 02, 24, 6])
     end
   end
 end
