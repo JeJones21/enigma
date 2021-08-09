776bcadd2ec7dd20cf3bac3230427269f7ba1dc0
Jessica Jones <daddehjones@gmail.com>
2021-08-08 21:03:06 -0400

    Extract character_set to follow law of demeter.

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,14 +1,14 @@
 require 'date'
 require './lib/enigma'
+# require_relative 'spec_helper'
 
 RSpec.describe Enigma do
   context 'initialize' do
     enigma = Enigma.new
+
     it "exists and has readable attributes" do
       expect(enigma).to be_a(Enigma)
-      expect(enigma.character_set.class).to eq(Array)
-      expect(enigma.character_set.length).to eq(27)
-    end
 
+    end
   end
 end
