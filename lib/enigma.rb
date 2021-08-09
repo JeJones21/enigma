717293cd6c0acf9623b7267391c5e62e7ece2745
Jessica Jones <daddehjones@gmail.com>
2021-08-09 17:56:47 -0400

    Transfer keys and offset to enigma class.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,9 +1,20 @@
 require 'date'
-class Enigma
+require './lib/encryptionable'
 
-  attr_reader :character_set
+class Enigma
+  include Encryptionable
+
+  attr_reader :offset,
+              :character_set
 
   def initialize
+    @offset = Offset.new
+    @character_set = ("a".."z").to_a << " "
+  end
+
+  def current_date
+    #this is a helper method
+    Date.today.strftime("%d%m%y")
   end
 
   def complete_shift(keys, offsets)
@@ -15,6 +26,29 @@ class Enigma
     shifts
   end
 
+  def keys(key)
+    shifts = []
+    shifts << key[0..1].to_i
+    shifts << key[1..2].to_i
+    shifts << key[2..3].to_i
+    shifts << key[3..4].to_i
+    shifts
+  end
+
+  def offset_shift(date)
+    square_date = (date.to_i) ** 2
+    string_array = square_date.to_s.split('')[-4..-1]
+    string_array.map do |string|
+      string.to_i
+    end
+    # @offset.last_four_strings.map { |string| string.to_i  }
+  end
+
+  def encrypt(message, key = randomized_key, date = current_date)
+    shifts = complete_shift(keys(key), offset_shift(date))
+    letters = message.downcase.split("")
+    encrypted_characters(letters, shifts, key, date)
+  end
   # def randomized_key
   #   5.times.map{rand(10)}.join
   # end
