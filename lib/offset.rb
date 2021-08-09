c26fe72d01bec2361843f135b22730056b3dddca
Jessica Jones <daddehjones@gmail.com>
2021-08-08 21:11:37 -0400

    Create current_date method.

--- lib/offset.rb
+++ lib/offset.rb
@@ -9,4 +9,9 @@ class Offset
     @date = date
     @character_set = ("a".."z").to_a << " "
   end
+
+  def current_date
+    #this is a helper method for offset.
+    Date.today.strftime("%d%m%y")
+  end
 end
