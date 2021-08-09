d90dc0aad7ef3ce4d398659fff18728269512689
Jessica Jones <daddehjones@gmail.com>
2021-08-09 17:55:12 -0400

    Create encrypt expectation.

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -15,5 +15,26 @@ RSpec.describe Enigma do
     it "can create a complete shift combining keys and offsets" do
       expect(enigma.complete_shift([40, 02, 20, 05], [4, 0, 4, 1])).to eq([44, 02, 24, 6])
     end
+
+    it "can encrypt a message with a date and key" do
+      allow(enigma).to receive(:current_date).and_return("090821")
+      allow(enigma).to receive(:randomized_key).and_return("01234")
+
+      expected1 = {
+                  encryption: "keder ohulw",
+                  key: "02715",
+                  date: "040895"
+                }
+
+      expected2 = {
+                  encryption: "keder ohulw!",
+                  key: "02715",
+                  date: "040895"
+                }
+
+    expect(enigma.encrypt("hello world", "02715", "040895")).to eq(expected1)
+    expect(enigma.encrypt("hello world!", "02715", "040895")).to eq(expected2)
+    expect(enigma.encrypt("pura vida", "01234", "090821")[:encryption]).to eq("ufriegilf")
+    end
   end
 end
