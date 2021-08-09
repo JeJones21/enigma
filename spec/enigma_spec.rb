887150cd89b5c77cd9ecd222bd770932ad1d8b08
Jessica Jones <daddehjones@gmail.com>
2021-08-09 18:27:09 -0400

    Create encryption test if date and key are not provided.

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -16,9 +16,9 @@ RSpec.describe Enigma do
       expect(enigma.complete_shift([40, 02, 20, 05], [4, 0, 4, 1])).to eq([44, 02, 24, 6])
     end
 
-    it "can encrypt a message with a date and key" do
-      allow(enigma).to receive(:current_date).and_return("090821")
-      allow(enigma).to receive(:randomized_key).and_return("01234")
+    it "can encrypt a message with a key and date" do
+      # allow(enigma).to receive(:current_date).and_return("090821")
+
 
       expected1 = {
                   encryption: "keder ohulw",
@@ -27,6 +27,12 @@ RSpec.describe Enigma do
                 }
 
       expect(enigma.encrypt("hello world", "02715", "040895")).to eq(expected1)
+    end
+
+    it "can use current date and randomized_key if not provided" do
+      allow(enigma).to receive(:current_date).and_return("090821")
+      allow(enigma).to receive(:randomized_key).and_return("01234")
+
       expect(enigma.encrypt("pura vida", "01234", "090821")[:encryption]).to eq("ufriegilf")
     end
 
@@ -46,7 +52,6 @@ RSpec.describe Enigma do
                   date: "040895"
                 }
       expect(enigma.decrypt("keder ohulw", "02715", "040895")).to eq(expected3)
-
     end
   end
 end
