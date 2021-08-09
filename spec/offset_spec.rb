1f0a43f45b5655340cda8ba285f650705ef69101
Jessica Jones <daddehjones@gmail.com>
2021-08-08 21:03:22 -0400

    Create character_set expectations.

--- spec/offset_spec.rb
+++ spec/offset_spec.rb
@@ -8,8 +8,10 @@ RSpec.describe Offset do
       expect(offset).to be_an_instance_of(Offset)
 
       expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
-      
+
       expect(offset.character_set).to eq(expected)
+      expect(enigma.character_set.class).to eq(Array)
+      expect(enigma.character_set.length).to eq(27)
     end
   end
 end
