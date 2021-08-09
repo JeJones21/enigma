575ed0772b6dfbb4789db15625610ec06159b76b
Jessica Jones <daddehjones@gmail.com>
2021-08-09 13:45:30 -0400

    Create complete_shift method.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,10 +4,16 @@ class Enigma
   attr_reader :character_set
 
   def initialize
-  
   end
 
-
+  def complete_shift(keys, offsets)
+    shifts = []
+    shifts << keys[0] + offsets[0]
+    shifts << keys[1] + offsets[1]
+    shifts << keys[2] + offsets[2]
+    shifts << keys[3] + offsets[3]
+    shifts
+  end
 
   # def randomized_key
   #   5.times.map{rand(10)}.join
